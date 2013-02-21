e3b4751d8c5b13a3a357e54205e4de03cf432ce4
Matt Revell <matthew.revell@lonelyplanet.com>
2013-02-21 14:20:24 +0000

    Task 5

--- basics/example.rb
+++ basics/example.rb
@@ -1,4 +1,11 @@
 class Article
     attr_reader :title, :body, :author, :created_at
     attr_accessor :likes, :dislikes
+
+    def initialize(title, body, author)
+        @title = title
+        @body = body
+        @author = author
+        @created_at = Time.now
+    end
 end
